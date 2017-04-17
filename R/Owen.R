97846b4eb0e40c4ff0fb74f1cd0d14bd7f6ef0b0
stla <laurent_step@yahoo.fr>
2017-04-17 22:51:20 +0200

    OwenT

--- R/Owen.R
+++ R/Owen.R
@@ -22,16 +22,16 @@
 .OwenT <- function(h, a){
   .C("owenTexport", h=as.double(h), a=as.double(a), result=numeric(1))$result
 }
-#' Student CDF with integer number of degrees of freedom
-#'
+
+#' @title Student CDF with integer number of degrees of freedom
+#' @description Cumulative distribution function of the noncentrel Student
+#' distribution with an integer number of degrees of freedom.
 #' @param q quantile
 #' @param nu integer greater than 1, the number of degrees of freedom
 #' @param delta noncentrality parameter
-#'
 #' @return Numeric value, the CDF evaluated at \code{q}.
 #' @export
 #' @useDynLib Owen
-#'
 #' @examples
 #' pStudent(2, 3)
 #' pt(2,3)
@@ -40,4 +40,23 @@ pStudent <- function(q, nu, delta=0){
     stop("`nu` must be an integer >1")
   }
   .pStudent(q=q, nu=nu, delta=delta)
+}
+
+#' @title Owen T-function
+#' @description Evaluates the Owen T-function by numerical integration.
+#' @param h numeric scalar
+#' @param a numeric scalar
+#' @return A number between 0 and 1.
+#' @export
+#' @useDynLib Owen
+#' @examples
+#' # theoretically 0:
+#' a <- runif(1, -1000, 1000)
+#' OwenT(0,a) - atan(a)/(2*pi)
+#' h <- runif(1, -3, 3)
+#' OwenT(h,1) - pnorm(h)*(1-pnorm(h))/2
+#' a <- 1000 # a -> Inf
+#' OwenT(h,a) - (1-pnorm(abs(h)))/2
+OwenT <- function(h, a){
+  .OwenT(h, a)
 }
