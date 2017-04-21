ad8980fd83a9d4a43090e7b6d9d1479029d24cae
stla <laurent_step@yahoo.fr>
2017-04-21 18:51:56 +0200

    update DLL

--- tests/testthat/test-OwenT.R
+++ tests/testthat/test-OwenT.R
@@ -17,7 +17,7 @@ test_that("OwenT(h,1)", {
 
 test_that("OwenT(h,Inf)", {
   h <- 1
-  expect_equal(OwenT(h,100), (1-pnorm(abs(h)))/2, tolerance=1e-12)
+  expect_equal(OwenT(h,100), (1-pnorm(abs(h)))/2, tolerance=1e-16)
 })
 
 test_that("Relation OwenT Cauchy", {
@@ -34,9 +34,8 @@ test_that("Comparison Mathematica", {
 test_that("Relation T(h,a) T(ah,1/a)", {
   h <- 0.5; a <- 2
   expect_equal(OwenT(h,a)+OwenT(a*h,1/a), (pnorm(h)+pnorm(a*h))/2-pnorm(h)*pnorm(a*h),
-               tolerance=1e-14)
+               tolerance=1e-15)
   a <- -2
-  expect_equal(OwenT(h,a)+OwenT(a*h,1/a),
-               (pnorm(h)+pnorm(a*h))/2-pnorm(h)*pnorm(a*h)-0.5,
-               tolerance=1e-14)
+  expect_true(OwenT(h,a)+OwenT(a*h,1/a) ==
+                (pnorm(h)+pnorm(a*h))/2-pnorm(h)*pnorm(a*h)-0.5)
 })
